3f98445ccffbc05e8ba49cb04d2bbf814dae7bf7
luisbytes <luisbytesdev@gmail.com>
2023-06-28 17:07:17 -0500

    Initial tabs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/ionic'],
+  routeRules: {
+    '/': { redirect: '/tabs/tab1' },
+    '/tabs': { redirect: '/tabs/tab1' },
+  },
   css: ["@/theme/variables.css"],
   ionic: {
     // https://ionic.nuxtjs.org/getting-started#options
---
+++ pages/tabs.vue
@@ -0,0 +1,23 @@
+<template>
+    <ion-page>
+        <ion-tabs>
+            <ion-router-outlet></ion-router-outlet>
+            <ion-tab-bar slot="bottom">
+                <ion-tab-button tab="tab1" href="/tabs/tab1">
+                    <ion-icon aria-hidden="true" :icon="ioniconsTriangle" />
+                    <ion-label>Tab 1</ion-label>
+                </ion-tab-button>
+
+                <ion-tab-button tab="tab2" href="/tabs/tab2">
+                    <ion-icon aria-hidden="true" :icon="ioniconsEllipse" />
+                    <ion-label>Tab 2</ion-label>
+                </ion-tab-button>
+
+                <ion-tab-button tab="tab3" href="/tabs/tab3">
+                    <ion-icon aria-hidden="true" :icon="ioniconsSquare" />
+                    <ion-label>Tab 3</ion-label>
+                </ion-tab-button>
+            </ion-tab-bar>
+        </ion-tabs>
+    </ion-page>
+</template>
